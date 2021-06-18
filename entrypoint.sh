a6eb14552c5574536b3ebc774d4a4a00be08c9b1
matang28 <4709399+matang28@users.noreply.github.com>
2021-06-18 03:54:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -l
 
-IMAGE_NAME="$INPUT_GCR_HOST$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
+IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 
-echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
+echo -n $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
+
+cd $INPUT_WORKING_DIR
+
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
